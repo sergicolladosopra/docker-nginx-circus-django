5ac69d37156de5c3c4ae14075644d569c5ee3431
Raul <rauropesop@gmail.com>
2015-11-04 18:05:09 +0100

    Docker compose to be used with postgres image

--- app/app/settings/pre.py
+++ app/app/settings/pre.py
@@ -3,13 +3,13 @@ from app.settings import *
 #INSTALLED_APPS += ('debug_toolbar',)
 
 
-#DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #'NAME': os.environ.get('OPENSHIFT_APP_NAME'),
-        #'USER': os.environ.get('OPENSHIFT_POSTGRESQL_DB_USERNAME'),
-        #'PASSWORD': os.environ.get('OPENSHIFT_POSTGRESQL_DB_PASSWORD'),
-        #'HOST': os.environ.get('OPENSHIFT_POSTGRESQL_DB_HOST'),
-        #'PORT': os.environ.get('OPENSHIFT_POSTGRESQL_DB_PORT'),
-    #}
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': postgres,
+        'USER': postgres,
+        'PASSWORD': postgres,
+        'HOST': db,
+        'PORT': 5432,
+    }
+}
