02c969349459ff73222482446d68f985b01a4341
sergicollado <sergicollado@gmail.com>
2015-11-04 17:54:17 +0100

    grappelli admin

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#pip install -r common.txt
+
+
+#if [ -z "$DJANGO_SETTINGS_MODULE" ]
+#then
+    #pip install -r dev.txt
+#else
+    #pip install -r pre.txt
+#fi
+
 python /opt/app/manage.py collectstatic --noinput
 python /opt/app/manage.py migrate
 nginx -c /code/config/nginx.conf
