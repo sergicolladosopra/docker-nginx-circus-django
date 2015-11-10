92353ad8086d561637ec9c99177a807e9f69a321
Raul <rauropesop@gmail.com>
2015-11-10 10:38:41 +0100

    Deleted docker-compose integration with postgres

--- app/app/settings/pre.py
+++ app/app/settings/pre.py
@@ -3,13 +3,13 @@ from app.settings import *
 INSTALLED_APPS += ('debug_toolbar',)
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': postgres,
-        'USER': postgres,
-        'PASSWORD': postgres,
-        'HOST': db,
-        'PORT': 5432,
-    }
-}
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': postgres,
+        # 'USER': postgres,
+        # 'PASSWORD': postgres,
+        # 'HOST': db,
+        # 'PORT': 5432,
+    # }
+# }
