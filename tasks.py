2e7cc0f8ca8da45503157a11e26fd050058ff3f0
Raul <rauropesop@gmail.com>
2015-11-11 11:00:45 +0100

    return to default values the names of the images

--- tasks.py
+++ tasks.py
@@ -2,21 +2,21 @@ from invoke import task, run
 
 
 @task
-def runserver(image="circusdjangoraul"):
+def runserver(image="djangorest"):
     env = '-e DJANGO_SETTINGS_MODULE="app.settings.dev"'
     command = "docker run -ti -v $PWD/app:/opt/app {env} --net=host {image}".format(image=image, env=env)
     print('exe: ',command)
     run(command)
 
 @task
-def runbash(image="circusdjangoraul"):
+def runbash(image="djangorest"):
     env = '-e DJANGO_SETTINGS_MODULE="app.settings.dev"'
     command = "docker run -ti -v $PWD/app:/opt/app {env} --net=host {image} /bin/bash".format(image=image, env=env)
     print('exe: ',command)
     run(command)
 
 @task
-def runtests(image="circusdjangoraul"):
+def runtests(image="djangorest"):
     command = "docker run -ti -v $PWD/app:/opt/app --net=host {image} /code/docker/run_tests.sh ".format(image=image)
     print('exe: ',command)
     run(command)
